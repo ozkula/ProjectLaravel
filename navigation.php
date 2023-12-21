d5b631e811966ea3048fa611821f6a34ccf571a9
RozaqIN-PCKAMPUS <at.ozkula@gmail.com>
2023-12-21 14:17:56 +0700

    first commit

---
+++ navigation.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg ftco-navbar-light" id="ftco-navbar" style="background: linear-gradient(180deg, #2BA500 55.73%, rgba(43, 165, 0, 0.00) 99.98%, rgba(43, 165, 0, 0.00) 99.99%);">
+	    <div class="container d-flex align-items-center">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+          <a class="navbar-brand" href="#" style="color:white">
+          <img src="assets/images/logors.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+          RSU BANYUMANIK 2
+            </a>
+	      <div class="collapse navbar-collapse justify-content-end" id="ftco-nav">
+	        <ul class="navbar-nav">
+	        	<li class="nav-item active"><a href="index.php" class="nav-link pl-0">Beranda</a></li>
+	        	<li class="nav-item"><a href="profile.php" class="nav-link">Profil</a></li>
+	        	<li class="nav-item"><a href="pelayanan.php" class="nav-link">Pelayanan</a></li>
+	        	<li class="nav-item"><a href="kontak.php" class="nav-link">Kontak</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
